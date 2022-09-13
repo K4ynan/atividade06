a670f261c3dab21d5df3b2536538e7746864db28
K4ynan <kaynancamilo12@gmail.com>
2022-09-12 21:02:02 -0300

    atividade06

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $("#conteudo").hide();
+
+    $("#link_abre").click(function() {
+
+        $("#conteudo").slideToggle("slow");
+    })
+})
+
+
+$(document).ready(function() {
+
+    $("#conteudo2").hide();
+
+    $("#link2").click(function() {
+
+        $("#conteudo2").slideToggle("slow");
+    })
+})
+
+
+$(document).ready(function() {
+
+    $("#conteudo3").hide();
+
+    $("#link3").click(function() {
+
+        $("#conteudo3").slideToggle("slow");
+    })
+})
